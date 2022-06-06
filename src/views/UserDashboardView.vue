4a1deec88351dda7ed66c86d40f7266d44afab1b
Windwalk <windwalk-bushido@protonmail.com>
2022-06-06 20:26:45 +0200

    Added two user pages

---
+++ src/views/UserDashboardView.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    methods: {
+      TriggerNavMenu() {
+        this.$root.ToggleMenu();
+      },
+    },
+
+    mounted() {
+      this.TriggerNavMenu();
+    },
+  };
+</script>
+
+<template>
+  <main>
+    <h1>About</h1>
+  </main>
+</template>
